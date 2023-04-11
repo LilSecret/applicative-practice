5897518fc28b889b1150c3979acfd649a6e882e9
Charles <chuckm248@gmail.com>
2023-04-11 12:13:16 -0600

    Get asteroid by name.

--- exercises/e10.js
+++ exercises/e10.js
@@ -5,7 +5,14 @@ import { data } from "../data/data";
 // Return example: { key1: value1, key2: value2, ... , keyN: valueN }
 
 export function getAsteroidDataByName(data, asteroidName) {
-  // Your code goes here...
+  const namedAstroid = data.asteroids
+    .filter((asteroid) => {
+      if(asteroid.name === asteroidName) {
+        return asteroid;
+      }
+    });
+
+  return namedAstroid[0]
 }
 
 
