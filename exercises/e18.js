b86457e1e00f8f4ce747dabf48177e87aa0ef324
Charles <chuckm248@gmail.com>
2023-04-15 21:21:05 -0600

    find the most common year asteroids were discovered

--- exercises/e18.js
+++ exercises/e18.js
@@ -1,3 +1,4 @@
+import { maxBy } from "../exercises/e17";
 /**
  * SPACE DATA EXERCISE 18
  * Return the year with the greatest number of Asteroids discoveries
@@ -6,7 +7,27 @@
 
 export function getGreatestDiscoveryYear(data) {
   // Your code goes here...
-  // feel free to import your `maxBy` or `minBy` methods from previous lessons
+  // I didn't know how to get the amount of times an asteroid was discovered in the years
+  // but I found a video that should me how to get the the most common item in an array
+  let asteroidYears = [];
+  for (let asteroid of data.asteroids) {
+    asteroidYears.push(asteroid.discoveryYear);
+  }
+  
+  const occur = {};
+  
+  asteroidYears.map((val) => {
+    occur[val] = occur[val] + 1 || 1;
+  });
+  
+  let year = Object.keys(occur);
+  let value = Object.values(occur);
+  let most = Math.max(...value);
+  let index = value.findIndex((val) => {
+    return val === most;
+  })
+  
+  return +year[index];
 }
 
 // === TEST YOURSELF ===
